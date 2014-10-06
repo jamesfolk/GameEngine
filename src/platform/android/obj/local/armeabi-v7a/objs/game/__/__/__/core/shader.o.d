6a67d65bed2cef6c542163c94ae1426b4cd1736d
james_folk <jamesfolk1@gmail.com>
2014-10-06 01:09:41 -0400

    added more files so that i can display a texture

---
+++ src/platform/android/obj/local/armeabi-v7a/objs/game/__/__/__/core/shader.o.d
@@ -0,0 +1,102 @@
+obj/local/armeabi-v7a/objs/game/__/__/__/core/shader.o: \
+ jni/../../../core/shader.c jni/../../../core/shader.h jni/platform_gl.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/GLES2/gl2.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/GLES2/gl2platform.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/KHR/khrplatform.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/api-level.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/stdint.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h \
+ jni/../../../platform/common/platform_log.h \
+ jni/../../../platform/common/platform_macros.h \
+ jni/../../../core/config.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/assert.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/stdlib.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/string.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/malloc.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/xlocale.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/alloca.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/strings.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/int-ll64.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/bitsperlong.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/bitsperlong.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/posix_types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/stddef.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/compiler.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/posix_types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/posix_types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/sysmacros.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/memory.h
+
+jni/../../../core/shader.h:
+
+jni/platform_gl.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/GLES2/gl2.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/KHR/khrplatform.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/api-level.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/stdint.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h:
+
+jni/../../../platform/common/platform_log.h:
+
+jni/../../../platform/common/platform_macros.h:
+
+jni/../../../core/config.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/assert.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/stdlib.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/string.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/malloc.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/xlocale.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/alloca.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/strings.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/int-ll64.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/bitsperlong.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/bitsperlong.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/posix_types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/stddef.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/compiler.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/posix_types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/posix_types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/sysmacros.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/memory.h:
---
+++ src/platform/android/obj/local/armeabi-v7a/objs/game/platform_asset_utils.o.d
@@ -0,0 +1,85 @@
+obj/local/armeabi-v7a/objs/game/platform_asset_utils.o: \
+ jni/platform_asset_utils.c \
+ jni/../../../platform/common/platform_asset_utils.h \
+ jni/../../../platform/common/platform_file_utils.h \
+ jni/../../../core/macros.h jni/../../../platform/common/platform_log.h \
+ jni/../../../platform/common/platform_macros.h \
+ jni/../../../core/config.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/asset_manager_jni.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/asset_manager.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/stdint.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/api-level.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/int-ll64.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/bitsperlong.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/bitsperlong.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/posix_types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/stddef.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/compiler.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/posix_types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/posix_types.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/sysmacros.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/jni.h \
+ /Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/assert.h
+
+jni/../../../platform/common/platform_asset_utils.h:
+
+jni/../../../platform/common/platform_file_utils.h:
+
+jni/../../../core/macros.h:
+
+jni/../../../platform/common/platform_log.h:
+
+jni/../../../platform/common/platform_macros.h:
+
+jni/../../../core/config.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/asset_manager_jni.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/asset_manager.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/stdint.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/android/api-level.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/int-ll64.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/bitsperlong.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/bitsperlong.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/posix_types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/stddef.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/linux/compiler.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm/posix_types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/asm-generic/posix_types.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/sys/sysmacros.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/jni.h:
+
+/Applications/adt-bundle-mac-x86_64-20131030/ndk/platforms/android-L/arch-arm/usr/include/assert.h:
